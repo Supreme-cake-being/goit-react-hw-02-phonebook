7c1362faf1dcb9f003f5cf81d0baf49b6ec96e37
supreme-cake-being <XGon20987@gmail.com>
2023-07-16 17:22:57 +0300

    fixing issues

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,10 +9,10 @@ import Filter from './Filter/Filter';
 class App extends Component {
   state = {
     contacts: [
-      { id: nanoid(), name: 'Serega Bublik', number: '228-69-47' },
-      { id: nanoid(), name: 'Harry Squater', number: '239-93-21' },
-      { id: nanoid(), name: 'Captain Jacked Sparrow', number: '432-34-56' },
-      { id: nanoid(), name: 'Harry Shprotter', number: '432-98-09' },
+      { id: "serega", name: 'Serega Bublik', number: '228-69-47' },
+      { id: "id-2", name: 'Harry Squater', number: '239-93-21' },
+      { id: "id-3", name: 'Captain Jacked Sparrow', number: '432-34-56' },
+      { id: "id-4", name: 'Harry Shprotter', number: '432-98-09' },
     ],
     filter: '',
   };
